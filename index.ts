b49f280313f494ad70a0a710d67e6745fb35ff59
Nbiba Bedis <bedisnbiba@gmail.com>
2023-05-28 21:59:34 +0100

    add !nvimhelp command

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import matrix from "npm:matrix-js-sdk";
 import { access_token, bot_user, homeserver } from "./config.ts";
+import stripAnsi from "npm:strip-ansi";
 
 const log = console.log;
 if (import.meta.main) {
@@ -25,18 +26,23 @@ if (import.meta.main) {
       const message: string | undefined = event.event.content?.body;
       log("room message:", message);
       if (message) {
+        let output;
         if (message.startsWith("!archwiki")) {
           log("looking in Arch Wiki");
-          const output = await arch_wiki(message.replace("!archwiki", ""));
-          log("output:", output);
-          if (output) {
-            const roomId = event.getRoomId();
-            if (roomId) {
-              await client.sendMessage(roomId, {
-                msgtype: "m.notice",
-                body: output,
-              });
-            }
+          output = await arch_wiki(message.replace("!archwiki", ""));
+        } else if (message.startsWith("!nvimhelp")) {
+          log("looking in nvim help");
+          output = await nvimHelp(message.replace("!nvimhelp", ""));
+        }
+
+        log("output:", output);
+        if (output) {
+          const roomId = event.getRoomId();
+          if (roomId) {
+            await client.sendMessage(roomId, {
+              msgtype: "m.notice",
+              body: output,
+            });
           }
         }
       }
@@ -58,4 +64,25 @@ async function arch_wiki(message: string): Promise<string | undefined> {
     .map((title: string) =>
       `https://wiki.archlinux.org/title/${encodeURIComponent(title)}`
     ).at(0);
+}
+
+async function nvimHelp(param: string) {
+  const cmd = new Deno.Command("bash", {
+    args: ["-c", `nvim -c ':help ${param}'`],
+    stdout: "piped",
+    stderr: "null",
+  }).spawn();
+  await new Promise((r) => setTimeout(r, 1000));
+
+  const buf = await cmd.stdout.getReader().read();
+
+  try {
+    cmd.kill();
+  } catch { /**/ }
+
+  return stripAnsi(new TextDecoder().decode(buf.value!))
+    .split(/\n/)
+    .slice(0, -1)
+    .join("\n")
+    .slice(6); // remove some ansi code that ddin't get stripped
 }
