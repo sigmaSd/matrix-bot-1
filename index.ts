f4b7e764dc6821db9d8d6d7dd33bd1d0ca5355e1
Nbiba Bedis <bedisnbiba@gmail.com>
2023-05-29 13:53:07 +0100

    some security

--- index.ts
+++ index.ts
@@ -78,8 +78,10 @@ async function arch_wiki(message: string): Promise<string | undefined> {
 }
 
 async function nvimHelp(param: string) {
+  const someSecurity = (p: string) => p.replaceAll("|", "");
+
   const cmd = new Deno.Command("bash", {
-    args: ["-c", `nvim -c ':help ${param}'`],
+    args: ["-c", `nvim -c ':help ${someSecurity(param)}'`],
     stdout: "piped",
     stderr: "null",
   }).spawn();
