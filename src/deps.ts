f9eff98b3f20bf7e483b1fa69a8838ff88f6fffb
Nbiba Bedis <bedisnbiba@gmail.com>
2023-06-09 21:27:43 +0100

    more refactor

--- src/deps.ts
+++ src/deps.ts
@@ -1,6 +1,6 @@
-export { $ } from "https://deno.land/x/dax@0.32.0/mod.ts";
 export { serve } from "https://deno.land/std@0.191.0/http/server.ts";
-export { parse } from "https://deno.land/std@0.170.0/flags/mod.ts";
+export { parse } from "https://deno.land/std@0.191.0/flags/mod.ts";
+export { ensureFile } from "https://deno.land/std@0.191.0/fs/ensure_file.ts";
 export { qrcode } from "https://deno.land/x/qrcode@v2.0.0/mod.ts";
-export { ensureFile } from "https://deno.land/std@0.182.0/fs/ensure_file.ts";
+export { $ } from "https://deno.land/x/dax@0.32.0/mod.ts";
 export * as matrix from "npm:matrix-js-sdk@26.0.1";
